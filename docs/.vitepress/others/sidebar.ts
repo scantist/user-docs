73953b05e86da0b5311ba39f7bcc6e96af044d44
Jackie <jwtan2010@gmail.com>
2023-07-04 10:43:34 +0800

    feat: add sbd parameters reference

--- docs/.vitepress/others/sidebar.ts
+++ docs/.vitepress/others/sidebar.ts
@@ -310,6 +310,16 @@ export function enUSSidebar() {
         },
       ]
     },
+    {
+      text: 'References',
+      collapsed: true,
+      items: [
+        {
+          text: 'Scantist BOM Detect CLI Commands & Parameters',
+          link: '/en-US/References/Scantist-BOM-Detect-CLI-Commands-&-Parameters',
+        },
+      ]
+    },
   ]
 }
 
